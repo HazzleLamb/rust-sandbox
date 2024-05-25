42e64c6047566ff4915b89ee5f893f88e3146f63
nevermind <nvrmd@nvrmd.ru>
2024-05-25 21:09:41 +0300

    replaced components with fat structures

--- src/main.rs
+++ src/main.rs
@@ -4,22 +4,24 @@ use nalgebra::{Rotation3, Vector3};
 
 use tracing_subscriber::fmt::{self, format::FmtSpan};
 
-struct Star {}
+struct Star {
+    pos: Vector3<f64>,
+}
 struct Planet {
     star_id: usize,
+    pos: Vector3<f64>,
+
+    tilt: Rotation3<f64>,
+    radius: f64,
+    period_secs: u64,
 }
 struct Moon {
     planet_id: usize,
-}
-
-struct Translation {
     pos: Vector3<f64>,
-}
 
-struct Orbit {
-    pub tilt: Rotation3<f64>,
-    pub radius: f64,
-    pub period_secs: u64,
+    tilt: Rotation3<f64>,
+    radius: f64,
+    period_secs: u64,
 }
 
 struct World {
@@ -27,8 +29,6 @@ struct World {
     stars: Vec<Option<Star>>,
     planet: Vec<Option<Planet>>,
     moon: Vec<Option<Moon>>,
-    translation: Vec<Option<Translation>>,
-    orbit: Vec<Option<Orbit>>,
 }
 
 pub const SECONDS_IN_HOUR: u64 = 60 * 60;
@@ -56,7 +56,7 @@ fn main() {
     // Используем функции, чтобы было удобно мерять время. Не выкабениваемся
     let mut world = generate_world();
 
-    let n_entities = world.translation.len();
+    let n_entities = world.stars.len();
     tracing::info!("Updating {} entities 10 times", n_entities);
     for _ in 0..10 {
         tick(&mut world)
@@ -67,62 +67,54 @@ fn main() {
 fn tick(world: &mut World) {
     world.tick_number += 1;
     // Звезды не обновляем, сразу к планетам
-    for (idx, planet_data) in world.planet.iter().enumerate() {
+    for planet_data in world.planet.iter_mut() {
         if let Some(planet_data) = planet_data {
             let host_pos = world
-                .translation
+                .stars
                 .get(planet_data.star_id)
                 .unwrap()
                 .as_ref()
                 .unwrap();
 
-            let planet_orbit = world.orbit.get(idx).unwrap().as_ref().unwrap();
-
-            let radius_vector = Vector3::new(planet_orbit.radius, 0.0, 0.0);
+            let radius_vector = Vector3::new(planet_data.radius, 0.0, 0.0);
             let orbit_turn_angle =
-                (2.0 * PI) / (world.tick_number % planet_orbit.period_secs) as f64;
+                (2.0 * PI) / (world.tick_number % planet_data.period_secs) as f64;
 
             let orbit_turn_rotation =
                 Rotation3::from_axis_angle(&Vector3::z_axis(), orbit_turn_angle);
 
             let rotated_radius_vector = orbit_turn_rotation.transform_vector(&radius_vector);
             let tilted_rotated_radius_vector =
-                planet_orbit.tilt.transform_vector(&rotated_radius_vector);
+                planet_data.tilt.transform_vector(&rotated_radius_vector);
 
             let pos = tilted_rotated_radius_vector + host_pos.pos;
 
-            let old_pos = world.translation.get_mut(idx).unwrap().as_mut().unwrap();
-            let _ = mem::replace(&mut old_pos.pos, pos);
+            let _ = mem::replace(&mut planet_data.pos, pos);
         }
     }
 
     // Аналогично - луны после планет
-    for (idx, moon_data) in world.moon.iter().enumerate() {
+    for moon_data in world.moon.iter_mut() {
         if let Some(moon_data) = moon_data {
             let host_pos = world
-                .translation
+                .planet
                 .get(moon_data.planet_id)
                 .unwrap()
                 .as_ref()
                 .unwrap();
 
-            let planet_orbit = world.orbit.get(idx).unwrap().as_ref().unwrap();
-
-            let radius_vector = Vector3::new(planet_orbit.radius, 0.0, 0.0);
-            let orbit_turn_angle =
-                (2.0 * PI) / (world.tick_number % planet_orbit.period_secs) as f64;
+            let radius_vector = Vector3::new(moon_data.radius, 0.0, 0.0);
+            let orbit_turn_angle = (2.0 * PI) / (world.tick_number % moon_data.period_secs) as f64;
 
             let orbit_turn_rotation =
                 Rotation3::from_axis_angle(&Vector3::z_axis(), orbit_turn_angle);
 
             let rotated_radius_vector = orbit_turn_rotation.transform_vector(&radius_vector);
             let tilted_rotated_radius_vector =
-                planet_orbit.tilt.transform_vector(&rotated_radius_vector);
+                moon_data.tilt.transform_vector(&rotated_radius_vector);
 
             let pos = tilted_rotated_radius_vector + host_pos.pos;
-
-            let old_pos = world.translation.get_mut(idx).unwrap().as_mut().unwrap();
-            let _ = mem::replace(&mut old_pos.pos, pos);
+            let _ = mem::replace(&mut moon_data.pos, pos);
         }
     }
 }
@@ -134,61 +126,45 @@ fn generate_world() -> World {
         stars: Vec::new(),
         planet: Vec::new(),
         moon: Vec::new(),
-        translation: Vec::new(),
-        orbit: Vec::new(),
     };
 
     for _ in 0..N_STARS {
         let star_id = world.stars.len();
 
-        world.stars.push(Some(Star {}));
-        world.planet.push(None);
-        world.moon.push(None);
-        world.translation.push(Some(Translation {
+        world.stars.push(Some(Star {
             pos: Vector3::new(0.0, 0.0, 0.0),
         }));
-        world.orbit.push(None);
+        world.planet.push(None);
+        world.moon.push(None);
 
         for _ in 0..N_PLANETS_PER_STAR {
             let planet_id = world.planet.len();
 
-            let orbit = generate_orbit();
-            let pos = generate_pos_of_orbiting(
-                world.translation.get(star_id).unwrap().as_ref().unwrap(),
-                &orbit,
-            );
-
             world.stars.push(None);
-            world.planet.push(Some(Planet { star_id }));
+            world.planet.push(Some(Planet {
+                star_id,
+                pos: Vector3::new(0.0, 0.0, 0.0),
+                tilt: generate_orbit_tilt(),
+                radius: fastrand::u64(100_000_000..100_000_000_000) as f64,
+                period_secs: fastrand::u64((3 * SECONDS_IN_HOUR)..(36 * SECONDS_IN_MONTH)),
+            }));
             world.moon.push(None);
-            world.translation.push(Some(pos));
-            world.orbit.push(Some(orbit));
 
             for _ in 0..N_MOONS_PER_PLANET {
-                let orbit = generate_orbit();
-                let pos = generate_pos_of_orbiting(
-                    world.translation.get(planet_id).unwrap().as_ref().unwrap(),
-                    &orbit,
-                );
-
                 world.stars.push(None);
                 world.planet.push(None);
-                world.moon.push(Some(Moon { planet_id }));
-                world.translation.push(Some(pos));
-                world.orbit.push(Some(orbit));
+                world.moon.push(Some(Moon {
+                    planet_id,
+                    pos: Vector3::new(0.0, 0.0, 0.0),
+                    tilt: generate_orbit_tilt(),
+                    radius: fastrand::u64(100_000_000..100_000_000_000) as f64,
+                    period_secs: fastrand::u64((3 * SECONDS_IN_HOUR)..(36 * SECONDS_IN_MONTH)),
+                }));
             }
         }
     }
 
     world
-}
-
-fn generate_orbit() -> Orbit {
-    Orbit {
-        tilt: generate_orbit_tilt(),
-        radius: fastrand::u64(100_000_000..100_000_000_000) as f64,
-        period_secs: fastrand::u64((3 * SECONDS_IN_HOUR)..(36 * SECONDS_IN_MONTH)),
-    }
 }
 
 fn generate_orbit_tilt() -> Rotation3<f64> {
@@ -197,14 +173,4 @@ fn generate_orbit_tilt() -> Rotation3<f64> {
     let yaw = fastrand::isize((ORBIT_AXIS_MAX_TILT.neg())..ORBIT_AXIS_MAX_TILT) as f64;
 
     Rotation3::from_euler_angles(roll, pitch, yaw)
-}
-
-fn generate_pos_of_orbiting(host_pos: &Translation, orbit: &Orbit) -> Translation {
-    let root_pos = orbit
-        .tilt
-        .transform_vector(&Vector3::new(orbit.radius, 0.0, 0.0));
-
-    let pos = root_pos + host_pos.pos;
-
-    Translation { pos }
 }
